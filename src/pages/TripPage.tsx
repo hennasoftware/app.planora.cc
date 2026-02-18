43c2d5ff357c3d6f34ef095b57c2e7b052d1bec0
Renato Teixeira <renato.gfteixeira@outlook.com>
2026-02-18 18:42:25 -0300

    refactor: rename ItineraryPage to TripPage and update to use useTrip hook and Trip data

--- src/pages/TripPage.tsx
+++ src/pages/TripPage.tsx
@@ -1,7 +1,7 @@
-import useItinerary from "../hooks/useItinerary.ts";
+import useTrip from "../hooks/useTrip.ts";
 
-export default function ItineraryPage() {
-  const { data, loading, error } = useItinerary("orlando-2026");
+export default function TripPage() {
+  const { trip, loading, notFound } = useTrip("test-orlando-2026-a1b1c3");
   let content;
 
   if (loading) {
@@ -10,33 +10,27 @@ export default function ItineraryPage() {
         <p>Loading itinerary...</p>
       </div>
     );
-  } else if (error) {
+  } else if (notFound || !trip) {
     content = (
       <div className="w-full max-w-xl overflow-hidden rounded-lg bg-white p-6">
-        <p>Error loading itinerary: {error}</p>
-      </div>
-    );
-  } else if (!data) {
-    content = (
-      <div className="w-full max-w-xl overflow-hidden rounded-lg bg-white p-6">
-        <p>No itinerary data available.</p>
+        <p>Error loading itinerary</p>
       </div>
     );
   } else {
     content = (
       <>
         <div className="w-full max-w-xl overflow-hidden rounded-lg bg-white p-6">
-          <h1>{data.location.city} Itinerary</h1>
-          <p>Start Date: {data.startDate}</p>
-          <p>End Date: {data.endDate}</p>
+          <h1>{trip.location.city} Itinerary</h1>
+          <p>Start Date: {trip.startDate}</p>
+          <p>End Date: {trip.endDate}</p>
         </div>
 
         <div className="w-full max-w-xl overflow-hidden rounded-lg bg-white p-6">
           <h2>Accommodation</h2>
-          <p>{data.accommodation.name}</p>
-          <p>{data.accommodation.address}</p>
+          <p>{trip.accommodation.name}</p>
+          <p>{trip.accommodation.address}</p>
           <a
-            href={data.accommodation.website}
+            href={trip.accommodation.website}
             className="text-blue-500 underline"
             target="_blank"
             rel="noopener noreferrer"
@@ -48,7 +42,7 @@ export default function ItineraryPage() {
         <div className="w-full max-w-xl overflow-hidden rounded-lg bg-white p-6">
           <h2>Places to Visit</h2>
           <ul>
-            {data.places.map((place) => (
+            {trip.places.map((place) => (
               <li key={place.id}>
                 <h3>{place.name}</h3>
                 <p>{place.address}</p>
